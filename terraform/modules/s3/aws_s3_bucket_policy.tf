887247d561b438b2583a31cc329e5197ddf99b6b
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-05-21 19:33:59 +0900

    add s3 policy

--- terraform/modules/s3/aws_s3_bucket_policy.tf
+++ terraform/modules/s3/aws_s3_bucket_policy.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket_policy" "allow_cloudfront" {
-  bucket = aws_s3_bucket.front.arn
+  bucket = aws_s3_bucket.front.id
   policy = data.aws_iam_policy_document.allow_cloudfront.json
 }
