48249917e84726830742e14f10cc4d8772d2950c
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-12 11:46:46 +0900

    add capacity provider

--- terraform/modules/ecs/aws_ecs_capacity_provider.tf
+++ terraform/modules/ecs/aws_ecs_capacity_provider.tf
@@ -4,12 +4,5 @@ resource "aws_ecs_capacity_provider" "nginx" {
   auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.nginx.arn
     managed_termination_protection = "ENABLED"
-
-    managed_scaling {
-      maximum_scaling_step_size = 1000
-      minimum_scaling_step_size = 1
-      status = "ENABLED"
-      target_capacity = 1
-    }
   }
 }
