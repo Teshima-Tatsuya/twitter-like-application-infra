2a570517cd9fee03929001b9d2d046762406824d
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-12 12:17:58 +0900

    add capacity providers

--- terraform/modules/ecs/aws_ecs_cluster_capacity_providers.tf
+++ terraform/modules/ecs/aws_ecs_cluster_capacity_providers.tf
@@ -1,6 +1,8 @@
 resource "aws_ecs_cluster_capacity_providers" "nginx" {
   cluster_name = aws_ecs_cluster.nginx.name
 
+  capacity_providers = [aws_ecs_capacity_provider.nginx.name]
+
   default_capacity_provider_strategy {
     base              = 1
     weight            = 100
