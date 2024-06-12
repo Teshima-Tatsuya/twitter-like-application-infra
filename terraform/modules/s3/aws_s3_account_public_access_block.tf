f1d66a990f76adab437699811753ed38c1e4cf04
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-12 15:36:22 +0900

    change public access block

--- terraform/modules/s3/aws_s3_account_public_access_block.tf
+++ terraform/modules/s3/aws_s3_account_public_access_block.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_account_public_access_block" "front" {
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
