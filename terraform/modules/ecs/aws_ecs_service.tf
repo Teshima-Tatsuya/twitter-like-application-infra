d60c4200a6b773575b6ab7bdba2d27fff565656a
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-13 16:23:31 +0900

    add ignore

--- terraform/modules/ecs/aws_ecs_service.tf
+++ terraform/modules/ecs/aws_ecs_service.tf
@@ -5,4 +5,17 @@ resource "aws_ecs_service" "all" {
   cluster = aws_ecs_cluster.twitter.arn
   task_definition = aws_ecs_task_definition.nginx.arn
   desired_count = 1
+
+  # 以下は毎回差分が出るため設定
+  lifecycle {
+    ignore_changes = [ 
+      health_check_grace_period,
+      propagate_tags,
+      tags,
+      capacity_provider_strategy,
+      deployment_circult_breader,
+      deployment_controller,
+    ]
+  }
+
 }
