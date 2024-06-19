b1a3c3431a2033d6243541a75813f45ed352cf12
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-19 17:50:37 +0900

    add role

--- terraform/modules/ecs/aws_ecs_task_definition.tf
+++ terraform/modules/ecs/aws_ecs_task_definition.tf
@@ -3,6 +3,7 @@ resource "aws_ecs_task_definition" "nginx" {
   container_definitions = file("${path.module}/task-difinitions/nginx.json")
   network_mode = "bridge"
   requires_compatibilities = ["EC2"]
+  task_role_arn = var.iam.role_ecsInstanceRole.arn
   runtime_platform {
     cpu_architecture = "ARM64"
   }
--- terraform/modules/iam/output.tf
+++ terraform/modules/iam/output.tf
@@ -1,6 +1,7 @@
 output "iam" {
   value = {
     "role" = aws_iam_role.nginx
+    "role_ecsInstanceRole" = aws_iam_role.ecsInstanceRole
     "profile" = aws_iam_instance_profile.nginx
   }
 }
