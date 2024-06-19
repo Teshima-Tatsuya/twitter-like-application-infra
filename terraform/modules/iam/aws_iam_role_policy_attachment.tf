ccd504356dcadda0d2fad3b03c89d88331afd02e
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-19 17:47:44 +0900

    add role

--- terraform/modules/iam/aws_iam_role_policy_attachment.tf
+++ terraform/modules/iam/aws_iam_role_policy_attachment.tf
@@ -3,6 +3,11 @@ resource "aws_iam_role_policy_attachment" "ecsInstanceRole-AmazonEC2ContainerSer
   policy_arn = data.aws_iam_policy.AmazonEC2ContainerServiceforEC2Role.arn
 }
 
+resource "aws_iam_role_policy_attachment" "ecsInstanceRole-AmazonSSMManagedInstanceCore" {
+  role       = aws_iam_role.ecsInstanceRole.name
+  policy_arn = data.aws_iam_policy.AmazonSSMManagedInstanceCore.arn
+}
+
 resource "aws_iam_role_policy_attachment" "nginx" {
   role       = aws_iam_role.nginx.name
   policy_arn = aws_iam_policy.nginx.arn
--- terraform/modules/iam/data.tf
+++ terraform/modules/iam/data.tf
@@ -2,6 +2,10 @@ data "aws_iam_policy" "AmazonEC2ContainerServiceforEC2Role" {
   name = "AmazonEC2ContainerServiceforEC2Role"
 }
 
+data "aws_iam_policy" "AmazonSSMManagedInstanceCore" {
+  name = "AmazonSSMManagedInstanceCore"
+}
+
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
