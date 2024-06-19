3c9190f0771e9d1dae0b97f89000b7cf6ecb6bc9
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-19 22:19:02 +0900

    fix task

--- terraform/modules/ecs/aws_ecs_task_definition.tf
+++ terraform/modules/ecs/aws_ecs_task_definition.tf
@@ -1,6 +1,9 @@
-resource "aws_ecs_task_definition" "nginx" {
-  family = "nginx"
-  container_definitions = file("${path.module}/task-difinitions/nginx.json")
+resource "aws_ecs_task_definition" "all" {
+  for_each = local.services
+  family = each.key
+  container_definitions = templatefile("${path.module}/task-difinitions/${each.key}.json", {
+    name = each.key,
+  })
   network_mode = "bridge"
   requires_compatibilities = ["EC2"]
   task_role_arn = var.iam.role_ecsInstanceRole.arn
