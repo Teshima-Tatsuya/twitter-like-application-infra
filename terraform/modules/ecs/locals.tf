c11708afdc7826a95958e8bd95ccd87cec9d44d6
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-21 16:41:40 +0900

    add rails

--- terraform/modules/ecs/locals.tf
+++ terraform/modules/ecs/locals.tf
@@ -3,10 +3,12 @@ locals {
     nginx = {
         service_name = "nginx-service"
         port_name = "http-80"
+        port = 80
     }
     rails = {
         service_name = "rails-service"
         port_name = "http-3000"
+        port = 3000
      }
   }
 }
