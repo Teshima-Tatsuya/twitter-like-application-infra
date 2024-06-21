781ec1b595abbd56ccd918f20b39478743c36cba
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-21 17:46:53 +0900

    add dynamic

--- terraform/modules/ecs/aws_ecs_cluster_capacity_providers.tf
+++ terraform/modules/ecs/aws_ecs_cluster_capacity_providers.tf
@@ -1,17 +1,13 @@
 resource "aws_ecs_cluster_capacity_providers" "all" {
   cluster_name = aws_ecs_cluster.twitter.name
 
-  capacity_providers = ["nginx"]
   # capacity_providers = keys(local.services)
 
-  # dynamic "default_capacity_provider_strategy" {
-  #   for_each = local.services
-  #   content {
-  #     capacity_provider = aws_ecs_capacity_provider.all[default_capacity_provider_strategy.key].name
-  #   }
-  # }
-  default_capacity_provider_strategy {
-    base = 1
-    capacity_provider = "nginx"
+  dynamic "default_capacity_provider_strategy" {
+    for_each = local.services
+    content {
+      weight = 1
+      capacity_provider = aws_ecs_capacity_provider.all[default_capacity_provider_strategy.key].name
+    }
   }
 }
