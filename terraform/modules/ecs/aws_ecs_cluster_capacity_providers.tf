70258fce6d0744ee32963667915d189d8661199e
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-21 17:14:10 +0900

    add dynamic

--- terraform/modules/ecs/aws_ecs_cluster_capacity_providers.tf
+++ terraform/modules/ecs/aws_ecs_cluster_capacity_providers.tf
@@ -6,8 +6,6 @@ resource "aws_ecs_cluster_capacity_providers" "all" {
   dynamic "default_capacity_provider_strategy" {
     for_each = local.services
     content {
-      base              = 1
-      weight            = 100
       capacity_provider = aws_ecs_capacity_provider.all[default_capacity_provider_strategy.key].name
     }
   }
