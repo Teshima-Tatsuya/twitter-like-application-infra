3a59f0157f39f212a514a333ea43939ccf6f2d9c
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-24 17:47:42 +0900

    fix userdata for NAT

--- terraform/modules/vpc/aws_network_interface.tf
+++ terraform/modules/vpc/aws_network_interface.tf
@@ -3,4 +3,6 @@ resource "aws_network_interface" "all" {
 
   subnet_id = aws_subnet.all["subnet-${each.value.subnet}"].id
   security_groups = [aws_security_group.all["sgp-${each.key}"].id]
+
+  source_dest_check = each.key == "nginx" ? false : true
 }
