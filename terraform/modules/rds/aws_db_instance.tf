135fffb9e734cf0ef8897087185697f5d747c77f
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-25 16:32:58 +0900

    fix engine name

--- terraform/modules/rds/aws_db_instance.tf
+++ terraform/modules/rds/aws_db_instance.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "main" {
   allocated_storage = 20
   instance_class = "t4g.micro"
-  engine = "postgresql"
+  engine = "postgres"
   username = "postgres"
   password = "postgres"
   max_allocated_storage = 0 # disable autoscaling
