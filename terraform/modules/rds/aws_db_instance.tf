059917dcd33978ffd8b8effcf3b9a3186c8937fb
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-25 16:23:12 +0900

    add rds

--- terraform/modules/rds/aws_db_instance.tf
+++ terraform/modules/rds/aws_db_instance.tf
@@ -2,7 +2,6 @@ resource "aws_db_instance" "main" {
   allocated_storage = 20
   instance_class = "t4g.micro"
   engine = "postgresql"
-  manage_master_user_password = true
   username = "postgres"
   password = "postgres"
   max_allocated_storage = 0 # disable autoscaling
